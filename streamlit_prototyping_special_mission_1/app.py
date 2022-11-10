9c4504de5050812ada2f0dd9c6b4993a4adf8078
LimePencil <jaeyoungshin03@gmail.com>
2022-11-10 16:31:07 +0900

    added camera support for deployment

--- streamlit_prototyping_special_mission_1/app.py
+++ streamlit_prototyping_special_mission_1/app.py
@@ -4,7 +4,22 @@ from PIL import Image
 import io
 from predict import load_model,predict_image
 
-
+def get_prediction(image_bytes):
+    image_bytes = image_bytes.getvalue()
+    image = Image.open(io.BytesIO(image_bytes))
+    columns=st.columns(3)
+    with columns[1]:
+        st.image(image, caption='Uploaded Image',use_column_width=True)
+    with st.spinner("Predicting Image..."):
+        y_hat = predict_image(model, image_bytes)
+    label = config['classes'][y_hat.item()]
+    col1,col2,col3=st.columns(3)
+    with col1:
+        st.metric("Gender",label[1])
+    with col2:
+        st.metric("Age",label[2])
+    with col3:
+        st.metric("Mask",label[0])
 
 st.title("Mask Classification Model")
 
@@ -16,21 +31,18 @@ model=load_model(config['model_path'],config['hparam_path'])
 
 st.markdown("<h6 style='text-align: right'> Model: "+str(type(model).__name__)+" </h6>",unsafe_allow_html=True)
 
-uploaded_file = st.file_uploader("Choose an image", type=["jpg", "jpeg","png"])
+genre = st.radio(
+    "Image Mode:",
+    ('Upload', 'Camera'))
 
-if uploaded_file:
-    image_bytes = uploaded_file.getvalue()
-    image = Image.open(io.BytesIO(image_bytes))
-    columns=st.columns(3)
-    with columns[1]:
-        st.image(image, caption='Uploaded Image',use_column_width=True)
-    y_hat = predict_image(model, image_bytes)
-    label = config['classes'][y_hat.item()]
-    col1,col2,col3=st.columns(3)
-    with col1:
-        st.metric("Gender",label[1])
-    with col2:
-        st.metric("Age",label[2])
-    with col3:
-        st.metric("Mask",label[0])
+if genre == 'Upload':
+    uploaded_file = st.file_uploader("Choose an image", type=["jpg", "jpeg","png"])
+    if uploaded_file:
+        get_prediction(uploaded_file)
+else:
+    picture = st.camera_input("Take a picture")
+    if picture:
+        get_prediction(picture)
+
+
 st.markdown("<h6 style='text-align: right; color: grey'> Created by Jaeyoung Shin </h6>",unsafe_allow_html=True)
